44795e72e76f508771758a06265edce59f78a521
jelenac11 <52001925+jelenac11@users.noreply.github.com>
2021-05-10 23:23:50 +0200

    body type rules changed

---
+++ better-me/src/main/java/better/me/facts/BodyTypeDTO.java
@@ -0,0 +1,16 @@
+package better.me.facts;
+
+import better.me.enums.BodyType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BodyTypeDTO {
+
+	private BodyType bodyType;
+}
--- better-me/src/test/java/tests/BodyTypeDeterminationTest.java
+++ better-me/src/test/java/tests/BodyTypeDeterminationTest.java
@@ -2,7 +2,7 @@ package tests;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -12,14 +12,14 @@ import org.kie.api.runtime.KieSession;
 
 import better.me.dto.BodyInfoDTO;
 import better.me.enums.BodyType;
-import better.me.rules.dto.BodyTypeDTO;
+import better.me.facts.Answers;
+import better.me.facts.BodyTypeDTO;
+import better.me.facts.Constants;
+import better.me.facts.UserAnswers;
 import better.me.util.MyLogger;
 
 public class BodyTypeDeterminationTest {
 
-	private static Integer ectoScore;
-    private static Integer endoScore;
-    private static Integer mesoScore;
     private final MyLogger myLogger = new MyLogger();
     
     protected final String ksessionName = "session";
@@ -31,27 +31,21 @@ public class BodyTypeDeterminationTest {
     	KieServices ks = KieServices.Factory.get();
         KieContainer kContainer = ks.newKieContainer(ks.newReleaseId("sbnz.integracija", "drools-spring-kjar", "0.0.1-SNAPSHOT"));
         kieSession = kContainer.newKieSession("session");
-		kieSession.getAgenda().getAgendaGroup("bodyType").setFocus();
+		kieSession.getAgenda().getAgendaGroup("body-type").setFocus();
 	}
     
     @Test
     public void bodyTypeRule_answersForEctomorphGiven_shouldDetermineEctomorphBodyType() {
         BodyInfoDTO infoDTO = new BodyInfoDTO("NARROWER THAN YOUR HIPS", "TIGHT AROUND YOUR GLUTES", "SMALL", "STAY SKINNY", "MOSTLY RECTANGLE", "FIND IT DIFFICULT TO GAIN AND MAINTAIN WEIGHT", "THE FINGERS OVERLAP");
         BodyTypeDTO bodyType = new BodyTypeDTO();
-        
-        getTypesScore(infoDTO);
-        		
-        assertEquals((Integer) 7, ectoScore);
-        assertEquals((Integer) 0, endoScore);
-        assertEquals((Integer) 0, mesoScore);
-        
-        kieSession.setGlobal("ectoScore", ectoScore);
-		kieSession.setGlobal("endoScore", endoScore);
-		kieSession.setGlobal("mesoScore", mesoScore);
+       
 		kieSession.setGlobal("myLogger", myLogger);
 	
-		kieSession.insert(infoDTO);
 		kieSession.insert(bodyType);
+		kieSession.insert(new UserAnswers(convertToMap(infoDTO)));
+		kieSession.insert(new Answers(BodyType.ECTOMORPH, Constants.ectoAnswers));
+		kieSession.insert(new Answers(BodyType.ENDOMORPH, Constants.endoAnswers));
+		kieSession.insert(new Answers(BodyType.MESOMORPH, Constants.mesoAnswers));
 		
         int firedRules = kieSession.fireAllRules();
         assertEquals(1, firedRules);
@@ -62,20 +56,14 @@ public class BodyTypeDeterminationTest {
     public void bodyTypeRule_answersForEndomorphGiven_shouldDetermineEndomorphBodyType() {
         BodyInfoDTO infoDTO = new BodyInfoDTO("WIDER THAN YOUR HIPS", "LOOSE AROUND YOUR GLUTES", "BIG", "CARRY SOME EXTRA FAT", "PEAR", "GAIN WEIGHT EASILY BUT HAVE A HARD TIME LOSING IT", "THE FINGERS DON'T TOUCH");
         BodyTypeDTO bodyType = new BodyTypeDTO();
-        
-        getTypesScore(infoDTO);
-        		
-        assertEquals((Integer) 0, ectoScore);
-        assertEquals((Integer) 7, endoScore);
-        assertEquals((Integer) 0, mesoScore);
-        
-        kieSession.setGlobal("ectoScore", ectoScore);
-		kieSession.setGlobal("endoScore", endoScore);
-		kieSession.setGlobal("mesoScore", mesoScore);
+
 		kieSession.setGlobal("myLogger", myLogger);
 	
-		kieSession.insert(infoDTO);
 		kieSession.insert(bodyType);
+		kieSession.insert(new UserAnswers(convertToMap(infoDTO)));
+		kieSession.insert(new Answers(BodyType.ECTOMORPH, Constants.ectoAnswers));
+		kieSession.insert(new Answers(BodyType.ENDOMORPH, Constants.endoAnswers));
+		kieSession.insert(new Answers(BodyType.MESOMORPH, Constants.mesoAnswers));
 		
         int firedRules = kieSession.fireAllRules();
         assertEquals(1, firedRules);
@@ -86,20 +74,14 @@ public class BodyTypeDeterminationTest {
     public void bodyTypeRule_answersForMesomorphGiven_shouldDetermineMesomorphBodyType() {
         BodyInfoDTO infoDTO = new BodyInfoDTO("SAME AS YOUR HIPS", "PERFECT AROUND YOUR GLUTES", "AVERAGE", "STAY FIT AND MUSCULAR", "HOURGLASS", "HAVE AN EASY TIME LOSING OR GAINING WEIGHT", "THE FINGERS JUST TOUCH");
         BodyTypeDTO bodyType = new BodyTypeDTO();
-        
-        getTypesScore(infoDTO);
         		
-        assertEquals((Integer) 0, ectoScore);
-        assertEquals((Integer) 0, endoScore);
-        assertEquals((Integer) 7, mesoScore);
-        
-        kieSession.setGlobal("ectoScore", ectoScore);
-		kieSession.setGlobal("endoScore", endoScore);
-		kieSession.setGlobal("mesoScore", mesoScore);
 		kieSession.setGlobal("myLogger", myLogger);
 	
-		kieSession.insert(infoDTO);
 		kieSession.insert(bodyType);
+		kieSession.insert(new UserAnswers(convertToMap(infoDTO)));
+		kieSession.insert(new Answers(BodyType.ECTOMORPH, Constants.ectoAnswers));
+		kieSession.insert(new Answers(BodyType.ENDOMORPH, Constants.endoAnswers));
+		kieSession.insert(new Answers(BodyType.MESOMORPH, Constants.mesoAnswers));
 		
         int firedRules = kieSession.fireAllRules();
         assertEquals(1, firedRules);
@@ -111,122 +93,31 @@ public class BodyTypeDeterminationTest {
         BodyInfoDTO infoDTO = new BodyInfoDTO("SAME AS YOUR HIPS", "LOOSE AROUND YOUR GLUTES", "BIG", "STAY SKINNY", "MOSTLY RECTANGLE", "FIND IT DIFFICULT TO GAIN AND MAINTAIN WEIGHT", "THE FINGERS OVERLAP");
         BodyTypeDTO bodyType = new BodyTypeDTO();
         
-        getTypesScore(infoDTO);
-        		
-        assertEquals((Integer) 0, ectoScore);
-        assertEquals((Integer) 0, endoScore);
-        assertEquals((Integer) 0, mesoScore);
-        
-        kieSession.setGlobal("ectoScore", ectoScore);
-		kieSession.setGlobal("endoScore", endoScore);
-		kieSession.setGlobal("mesoScore", mesoScore);
 		kieSession.setGlobal("myLogger", myLogger);
 	
-		kieSession.insert(infoDTO);
 		kieSession.insert(bodyType);
+		kieSession.insert(new UserAnswers(convertToMap(infoDTO)));
+		kieSession.insert(new Answers(BodyType.ECTOMORPH, Constants.ectoAnswers));
+		kieSession.insert(new Answers(BodyType.ENDOMORPH, Constants.endoAnswers));
+		kieSession.insert(new Answers(BodyType.MESOMORPH, Constants.mesoAnswers));
 		
         int firedRules = kieSession.fireAllRules();
         assertEquals(0, firedRules);
         assertEquals(null, bodyType.getBodyType());
     }
     
-    private void getTypesScore(BodyInfoDTO dto) {
-		ArrayList<String> endoAnswers = new ArrayList<String>() {
+    private HashMap<String, String> convertToMap(BodyInfoDTO dto) {
+    	return new HashMap<String, String>() {
 			private static final long serialVersionUID = 1L;
-
-			{
-                add("PEAR");
-                add("CARRY SOME EXTRA FAT");
-                add("THE FINGERS DON'T TOUCH");
-                add("BIG");
-                add("LOOSE AROUND YOUR GLUTES");
-                add("WIDER THAN YOUR HIPS");
-                add("GAIN WEIGHT EASILY BUT HAVE A HARD TIME LOSING IT");
-            }
-        };
-		ArrayList<String> ectoAnswers = new ArrayList<String>() {
-			private static final long serialVersionUID = 1L;
-
-			{
-                add("MOSTLY RECTANGLE");
-                add("STAY SKINNY");
-                add("THE FINGERS OVERLAP");
-                add("SMALL");
-                add("TIGHT AROUND YOUR GLUTES");
-                add("NARROWER THAN YOUR HIPS");
-                add("FIND IT DIFFICULT TO GAIN AND MAINTAIN WEIGHT");
-            }
-        };
-        ArrayList<String> mesoAnswers = new ArrayList<String>() {
-			private static final long serialVersionUID = 1L;
-
-			{
-                add("HOURGLASS");
-                add("STAY FIT AND MUSCULAR");
-                add("THE FINGERS JUST TOUCH");
-                add("AVERAGE");
-                add("PERFECT AROUND YOUR GLUTES");
-                add("SAME AS YOUR HIPS");
-                add("HAVE AN EASY TIME LOSING OR GAINING WEIGHT");
-            }
-        };
-        ectoScore = 0;
-        endoScore = 0;
-        mesoScore = 0;
-        if (endoAnswers.contains(dto.getBodyLook()))
-        	endoScore += 1;
-        if (ectoAnswers.contains(dto.getBodyLook()))
-        	ectoScore += 1;
-        if (mesoAnswers.contains(dto.getBodyLook()))
-        	mesoScore += 1;
-        
-        if (endoAnswers.contains(dto.getBodyTendation()))
-        	endoScore += 1;
-        if (ectoAnswers.contains(dto.getBodyTendation()))
-        	ectoScore += 1;
-        if (mesoAnswers.contains(dto.getBodyTendation()))
-        	mesoScore += 1;
-        
-        if (endoAnswers.contains(dto.getForearms()))
-        	endoScore += 1;
-        if (ectoAnswers.contains(dto.getForearms()))
-        	ectoScore += 1;
-        if (mesoAnswers.contains(dto.getForearms()))
-        	mesoScore += 1;
-        
-        if (endoAnswers.contains(dto.getEncircleHandWrist()))
-        	endoScore += 1;
-        if (ectoAnswers.contains(dto.getEncircleHandWrist()))
-        	ectoScore += 1;
-        if (mesoAnswers.contains(dto.getEncircleHandWrist()))
-        	mesoScore += 1;
-        
-        if (endoAnswers.contains(dto.getJeans()))
-        	endoScore += 1;
-        if (ectoAnswers.contains(dto.getJeans()))
-        	ectoScore += 1;
-        if (mesoAnswers.contains(dto.getJeans()))
-        	mesoScore += 1;
-        
-        if (endoAnswers.contains(dto.getShoulders()))
-        	endoScore += 1;
-        if (ectoAnswers.contains(dto.getShoulders()))
-        	ectoScore += 1;
-        if (mesoAnswers.contains(dto.getShoulders()))
-        	mesoScore += 1;
-        
-        if (endoAnswers.contains(dto.getWeightTendation()))
-        	endoScore += 1;
-        if (ectoAnswers.contains(dto.getWeightTendation()))
-        	ectoScore += 1;
-        if (mesoAnswers.contains(dto.getWeightTendation()))
-        	mesoScore += 1;
-        
-        if (ectoScore * endoScore * mesoScore != 0 ) {
-        	ectoScore = 0;
-            endoScore = 0;
-            mesoScore = 0;
-        } 
-	}
+		{
+	        put("shoulders", dto.getShoulders());
+	        put("jeans", dto.getJeans());
+	        put("forearms", dto.getForearms());
+	        put("bodyTendation", dto.getBodyTendation());
+	        put("bodyLook", dto.getBodyLook());
+	        put("weightTendation", dto.getWeightTendation());
+	        put("encircleHandWrist", dto.getEncircleHandWrist());
+	    }};
+    }
     
 }
