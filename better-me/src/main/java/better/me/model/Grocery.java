3ca5e75b28f27d32220a7bdf4e545c788ae227e5
jelenac11 <52001925+jelenac11@users.noreply.github.com>
2021-05-17 16:36:50 +0200

    Filter meal rules

--- better-me/src/main/java/better/me/model/Grocery.java
+++ better-me/src/main/java/better/me/model/Grocery.java
@@ -1,5 +1,6 @@
 package better.me.model;
 
+import better.me.enums.Diet;
 import better.me.modelDB.GroceryDB;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,13 +15,14 @@ public class Grocery {
 
 	private Long id;
 	private String name;
+	private Diet diet;
 	private double calories;
 	private double carbs;
 	private double proteins;
 	private double fats;
 	
 	public Grocery(GroceryDB g) {
-		this(g.getId(), g.getName(), g.getCalories(), g.getCarbs(), g.getProteins(), g.getFats());
+		this(g.getId(), g.getName(), g.getDiet(), g.getCalories(), g.getCarbs(), g.getProteins(), g.getFats());
 	}
 
 }
